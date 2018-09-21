17a683e7ae61de9943cccfc35e60876e8da1a6ea
cactusism <reem.alrashoud@gmail.com>
2018-09-21 19:17:25 +0300

    version1

--- app/src/main/java/com/software444/ssww444/MainActivity.java
+++ app/src/main/java/com/software444/ssww444/MainActivity.java
@@ -2,15 +2,17 @@ package com.software444.ssww444;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mSendData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+      //  Firebase.setAndroidContext(this);
 
+        mSendData = (Button) findViewById(R.id.sendData);
+
+    }
 }
