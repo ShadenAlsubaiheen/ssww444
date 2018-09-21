3a8b65ba8b58eb3208a529908f3ee4c783159969
shaden <shaden.51@hotmail.com>
2018-09-21 19:10:53 +0300

    commit12

---
+++ app/src/main/java/com/software444/ssww444/HomePageMother.java
@@ -0,0 +1,13 @@
+package com.software444.ssww444;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePageMother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_mother);
+    }
+}
