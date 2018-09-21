b2a5e2ea074f8e502217de449aa36b8a6aef7c36
cactusism <reem.alrashoud@gmail.com>
2018-09-21 19:16:57 +0300

    1st v

---
+++ app/src/main/java/com/software444/ssww444/signUpActivity.java
@@ -0,0 +1,23 @@
+package com.software444.ssww444;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class signUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        findViewById(R.id.textViewSignup).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
